90f4bb6a3c4b731b1549d6c1c1dbeca9603aa4a7
Howard Liberty <liberty@anymemo.org>
2014-02-23 12:34:37 -0800

    Initial import

---
+++ WordWall-desktop/src/org/liberty/android/wordwall/Main.java
@@ -0,0 +1,16 @@
+package org.liberty.android.wordwall;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "WordWall";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new WordWall(), cfg);
+	}
+}
