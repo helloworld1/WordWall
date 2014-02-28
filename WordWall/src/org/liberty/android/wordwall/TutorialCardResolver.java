82a93e28f0e0374135b83bcdf08237533d16a816
Howard Liberty <liberty@anymemo.org>
2014-02-27 17:16:14 -0800

    Add more cards for the default display

--- WordWall/src/org/liberty/android/wordwall/TutorialCardResolver.java
+++ WordWall/src/org/liberty/android/wordwall/TutorialCardResolver.java
@@ -16,11 +16,13 @@ public class TutorialCardResolver implements CardResolver {
     private final List<Card> mCards = new ArrayList<Card>(10);
 
     public TutorialCardResolver() {
-        mCards.add(new Card("What is WordWall",
-                "It's a wallpaper/daydream that shows cards from AnyMemo."));
+        mCards.add(new Card("What is WordWall", "It's a wallpaper/daydream that shows cards from AnyMemo."));
         mCards.add(new Card("How to use wallpaper", "Set the live wallpaper in Android's home screen."));
         mCards.add(new Card("How to use daydream", "Go to Android's Settings and set daydream in 'display'"));
         mCards.add(new Card("Download AnyMemo", "Go to Google Play and Search 'AnyMemo'"));
+        mCards.add(new Card("Select card database", "Use settings in livewallpaper or daydream"));
+        mCards.add(new Card("Get more cards ", "Use AnyMemo to download/create databases"));
+        mCards.add(new Card("Create new cards", "Use AnyMemo to create new cards"));
     }
 
     @Override
