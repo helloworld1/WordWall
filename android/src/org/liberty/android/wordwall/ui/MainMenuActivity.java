bd23b2c7dc59c1eea9aafc1e09dfa9d6add9f95a
Howard Liberty <liberty@anymemo.org>
2014-05-15 18:34:07 -0700

    Use the better livewallpaper chooser.

--- android/src/org/liberty/android/wordwall/ui/MainMenuActivity.java
+++ android/src/org/liberty/android/wordwall/ui/MainMenuActivity.java
@@ -9,17 +9,11 @@ import java.util.List;
 
 import org.liberty.android.wordwall.MainActivity;
 import org.liberty.android.wordwall.R;
-import org.liberty.android.wordwall.R.drawable;
-import org.liberty.android.wordwall.R.id;
-import org.liberty.android.wordwall.R.layout;
-import org.liberty.android.wordwall.R.string;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.app.WallpaperManager;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 /**
  * This is used in Android to launch from launcher icon.
@@ -44,7 +38,8 @@ public class MainMenuActivity extends Activity {
         Intent launchDaydreamIntent = new Intent(android.provider.Settings.ACTION_DREAM_SETTINGS);
         Card daydreamCard = new MenuCard(this, R.string.daydream_card_header, R.string.daydream_card_text, R.drawable.dragon, launchDaydreamIntent);
 
-        Intent launchLiveWallpaperIntent = new Intent(android.provider.Settings.ACTION_DISPLAY_SETTINGS);
+        Intent launchLiveWallpaperIntent = new Intent();
+        launchLiveWallpaperIntent.setAction(WallpaperManager.ACTION_LIVE_WALLPAPER_CHOOSER);
         Card livewallpaperCard = new MenuCard(this, R.string.livewallpaper_card_header, R.string.livewallpaper_card_text, R.drawable.lizard, launchLiveWallpaperIntent);
 
         CardListView cardListView = (CardListView) findViewById(R.id.menu_card_list);
